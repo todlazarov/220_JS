128c40f76d816e395ee8f4b2047875311d0980cf
Tod Lazarov <tod.lazarov1@gmail.com>
2016-06-16 14:23:10 -0400

    Tabs functionallity with jQuery

---
+++ Lesson_4/tabs/tabs.js
@@ -0,0 +1,8 @@
+$(function() {
+
+  $("a").on("click", function(e) {
+    e.preventDefault();
+
+    $("article").hide().filter("[data-block=" + $(this).attr("data-block") + "]").show();
+  });
+});
