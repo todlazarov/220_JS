de08622be9aea45aa04a3cba940870055f9c737b
Tod Lazarov <tod.lazarov1@gmail.com>
2016-06-22 20:47:26 -0400

    Changes

--- Lesson_6/gallery_server/public/javascripts/gallery.js
+++ Lesson_6/gallery_server/public/javascripts/gallery.js
@@ -3,7 +3,7 @@ $(function() {
   var templates = {},
       photos;
 
-  $("script[type='text/x-handlerbars']").each(function() {
+  $("script[type='text/x-handlebars']").each(function() {
     var $tmpl = $(this);
     templates[$tmpl.attr("id")] = Handlebars.compile($tmpl.html());
   });
@@ -12,6 +12,7 @@ $(function() {
     url: "/photos",
     success: function(json) {
       photos = json;
+      console.dir(photos);
       $("#slides").html(templates.photos({ photos: photos }));
       $("section > header").html(templates.photo_information(photos[0]));
     }
